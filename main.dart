1778f38f786caf7ef393c9f57b433ec21fc69662
João Ribeiro <joao.rosado.ribeiro@gmail.com>
2023-04-17 18:15:51 +0100

    Unsolved

--- main.dart
+++ main.dart
@@ -1,3 +1,14 @@
+int? couldReturnNullButDoesnt() => -3;
+
 void main() {
-  print("Hello World!");
+  int? couldBeNullButIsnt = 1;
+  List<int?> listThatCouldHoldNulls = [2, null, 4];
+
+  int a = couldBeNullButIsnt;
+  int b = listThatCouldHoldNulls.first; // first item in the list
+  int c = couldReturnNullButDoesnt().abs(); // absolute value
+
+  print('a is $a.');
+  print('b is $b.');
+  print('c is $c.');
 }
