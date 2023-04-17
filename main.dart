4f023290037f1cdd573e7727bf39efb3def7726f
João Ribeiro <joao.rosado.ribeiro@gmail.com>
2023-04-17 20:16:45 +0100

    Unsolved

--- main.dart
+++ main.dart
@@ -1,3 +1,16 @@
+int _computeValue() {
+  print('In _computeValue...');
+  return 3;
+}
+
+class CachedValueProvider {
+  final _cache = _computeValue();
+  int get value => _cache;
+}
+
 void main() {
-  print("Hello World!");
+  print('Calling constructor...');
+  var provider = CachedValueProvider();
+  print('Getting value...');
+  print('The value is ${provider.value}!');
 }
